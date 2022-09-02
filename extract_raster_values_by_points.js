2dec9cc76b3704f28c32ee1c8cc7669b14fc972f
Neli <neliquinhonez92@gmail.com>
2022-09-02 06:03:30 -0300

    Update extract_raster_values_by_points.js

--- extract_raster_values_by_points.js
+++ extract_raster_values_by_points.js
@@ -1,25 +1,44 @@
 
-// Link to the code: https://code.earthengine.google.com/5ff32a17a4fde7900f03d42e4107e293
+// Link to the code: https://code.earthengine.google.com/854bd58026e79d5e171ce1e6c4da8c94
 
-// Add map
-Map.centerObject(pts)
+//Add layers to the map
+Map.addLayer(DEM, {min: 100, max: 250, palette: ['ff0a0a','ffec06','82ff10','194bff']}, 'DEM');
 Map.addLayer(pts)
+Map.centerObject(pts);
+
+// Get the DEM values of the points
+var pts_raster = DEM.reduceRegions({
+                    collection: pts, 
+                    reducer: ee.Reducer.mean(), 
+                    scale: 15}); // define a scale in meters
+
+print(pts_raster);
+
+//Export to 
+Export.table.toDrive({
+  collection: pts_raster, 
+  description: 'pts_dem', 
+  fileFormat: 'CSV', 
+});
 
 // Importing module
 var TAGEE = require('users/joselucassafanelli/TAGEE:TAGEE-functions');
 
-var DEMAttributes = TAGEE.terrainAnalysis(TAGEE, dem, pts);
+var DEMAttributes = TAGEE.terrainAnalysis(TAGEE, DEM, pts);
 print(DEMAttributes.bandNames(), 'Parameters of Terrain');
 
-// get the slope of the points
+// Get all the values of the RaserStack of the points
 var terrain = DEMAttributes.reduceRegions({
                     collection: pts, 
-                    reducer: ee.Reducer.first(), 
-                    scale: 30}); // define a scale in meters
+                    reducer: ee.Reducer.mean(), 
+                    scale: 15}); // define a scale in meters
 
 print(terrain);
+
+//Export to drive
 Export.table.toDrive({
   collection: terrain, 
-  description: 'terrain', 
+  description: 'terrain_pts_all', 
+  //selectors: ['Experiment', 'Aspect', 'Slope', 'Eastness', 'Northness'],
   fileFormat: 'CSV', 
 });
